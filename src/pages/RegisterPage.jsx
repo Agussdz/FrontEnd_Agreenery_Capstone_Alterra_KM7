8d38cd786eca5a99f3a00fcf5f898d3c77600ee7
Agussdz <agussutiyanto07@gmail.com>
2024-12-03 00:15:39 +0700

    Add register feature

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
 import loginBg from "../assets/login-bg.png";
 import logoBg from "../assets/login-logo-bg.png";
+import useRegister from "../hooks/useRegister";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
+  const {
+    formData,
+    setFormData,
+    loading,
+    setLoading,
+    handleInputChange,
+    handleSubmit,
+  } = useRegister();
+
   return (
     <div className="min-h-screen flex items-center bg-gray-100">
       {/* Left Section (Background Image) */}
-      <div className="relative w-[712px] h-[1024px]">
+      <div className="relative hidden lg:block w-full max-w-[712px] h-[740px]">
         {/* Background Image */}
         <img
           src={loginBg}
@@ -15,30 +25,13 @@ export default function RegisterPage() {
         />
 
         {/* Container for Logo and Text */}
-        <div
-          className="absolute top-[263px] left-[64px] flex flex-col gap-[10px]"
-          style={{ width: "500px", height: "311px", opacity: 1 }}
-        >
-          {/* Logo Image */}
+        <div className="absolute top-[153px] left-[64px] flex flex-col gap-2 w-[500px] h-[311px]">
           <img
             src={logoBg}
             alt="Logo"
             className="w-full h-auto object-contain"
           />
-
-          {/* Text and logo */}
-          <p
-            className="text-[#FFFFFF] text-center"
-            style={{
-              fontFamily: "Roboto",
-              fontSize: "24px",
-              fontWeight: 400,
-              lineHeight: "28px",
-              textAlign: "center",
-              textUnderlinePosition: "from-font",
-              textDecorationSkipInk: "none",
-            }}
-          >
+          <p className="text-neutral-100 text-center text-2xl font-roboto-400">
             Selamat datang di Agreenery, platform
             <br />
             pertanian terintegrasi yang membantu <br />
@@ -47,98 +40,102 @@ export default function RegisterPage() {
         </div>
       </div>
 
-      {/* Right Section (Empty or Form Content) */}
-      <div className="min-h-screen flex flex-col justify-start p-10">
-        {/* Placeholder for other content */}
-        <div style={{ marginTop: "128px", marginLeft: "168px" }}>
-          <h1
-            className="text-left text-xl"
-            style={{
-              fontFamily: "Roboto, sans-serif",
-              fontSize: "48px",
-              fontWeight: 700,
-              lineHeight: "60px",
-              textAlign: "left",
-              textUnderlinePosition: "from-font",
-              textDecorationSkipInk: "none",
-            }}
-          >
-            <span className="text-primary-400">Create</span> <br />
-            your account
+      {/* Right Section (Form Content) */}
+      <div className="flex flex-col justify-center px-8 sm:px-16 lg:px-24 py-10 w-full">
+        <div className="mx-3 lg:mx-[90px] ">
+          <h1 className="text-4xl font-roboto-700 text-left text-neutral-500 mb-10">
+            <span className="text-primary-500">Create</span> your <br />
+            account
           </h1>
-          <form className="max-w-sm ml-0" style={{ paddingTop: "32px" }}>
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="mb-5">
               <label
-                for="name"
-                className="font-roboto-700 text-neutral-400 opacity-50"
+                htmlFor="display_name"
+                className="block text-neutral-400 text-sm font-medium"
               >
                 Nama
               </label>
               <input
-                placeholder="Masukkan Nama"
                 type="text"
-                id="name"
-                className="text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 :border- bg-neutral-200 opacity-50"
+                id="display_name"
+                name="display_name"
+                value={formData.display_name}
+                onChange={handleInputChange}
+                className="mt-1 w-full px-4 py-3 rounded-lg border border-neutral-300 bg-neutral-200 text-sm text-neutral-700 placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-primary-300"
+                placeholder="Masukkan Nama"
                 required
               />
             </div>
             <div className="mb-5">
               <label
-                for="email"
-                className="font-roboto-700 text-neutral-400 opacity-50"
+                htmlFor="email"
+                className="block text-neutral-400 text-sm font-medium"
               >
                 Email
               </label>
               <input
                 type="email"
                 id="email"
-                className="text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 :border- bg-neutral-200 opacity-50"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className="mt-1 w-full px-4 py-3 rounded-lg border border-neutral-300 bg-neutral-200 text-sm text-neutral-700 placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-primary-300"
                 placeholder="user@gmail.com"
                 required
               />
             </div>
-
             <div className="mb-5">
               <label
-                for="name"
-                className="font-roboto-700 text-neutral-400 opacity-50"
+                htmlFor="phone"
+                className="block text-neutral-400 text-sm font-medium"
               >
                 Nomor Telepon
               </label>
               <input
-                placeholder="Nomor Telepon"
-                type="number"
+                type="tel"
                 id="phone"
-                className="text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 :border- bg-neutral-200 opacity-50"
+                name="phone"
+                value={formData.phone}
+                onChange={handleInputChange}
+                className="mt-1 w-full px-4 py-3 rounded-lg border border-neutral-300 bg-neutral-200 text-sm text-neutral-700 placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-primary-300"
+                placeholder="Nomor Telepon"
                 required
               />
             </div>
-
-            <div className="mb-5">
+            <div className="mb-5 relative">
               <label
-                for="name"
-                className="font-roboto-700 text-neutral-400 opacity-50"
+                htmlFor="password"
+                className="block text-neutral-400 text-sm font-medium"
               >
                 Password
               </label>
-              <input
-                placeholder="Input Password"
-                type="password"
-                id="password"
-                className="text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 :border- bg-neutral-200 opacity-50"
-                required
-              />
+              <div className="relative">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className="mt-1 w-full px-4 py-3 rounded-lg border border-neutral-300 bg-neutral-200 text-sm text-neutral-700 placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-primary-300"
+                  placeholder="Masukkan Password"
+                  required
+                />
+              </div>
             </div>
 
             <button
-              type="button"
-              className="w-full bg-primary-300 text-white font-semibold py-3 rounded-lg hover:bg-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-400 mt-4 text-neutral-100"
+              type="submit"
+              className="w-full py-3 bg-primary-500 text-neutral-100 font-semibold rounded-lg hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-400"
+              disabled={loading}
             >
-              Sign Up
+              {loading ? "Loading..." : "Sign Up"}
             </button>
-            <p className=" text-center pt-4">
-              Already have an account?
-              <span className=" text-secondary-500"> Sign In</span>
+
+            <p className="text-center pt-4 text-sm">
+              Already have an account?{" "}
+              <Link to={"/login"}>
+                <span className="text-primary-500">Sign in</span>
+              </Link>
             </p>
           </form>
         </div>
